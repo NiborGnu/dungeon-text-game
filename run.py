7a4761b9d8d76612be30394dac1bf384ccdfab8a
Robin Ung <Robin_ung@hotmail.com>
2024-01-09 02:18:06 +0000

    Add return value to difficulty, Add player values, Fixed spelling

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-# Text based Game RPG
+# Text based RPG Game
 # Made By Robin Ung 
 
 import random
 import time
 
 
-class player():
+# Player 
+class Player():
     """
     Player setup
     """
     def __init__(self, name, hp):
-        self.name = ''
-        self.hp = 0
+        self.name = name
+        self.hp = hp
 
 
 def get_player_name():
@@ -31,7 +32,11 @@ def get_player_name():
             print('Do you have a nickname i can call you?\n')
 
 
+# Difficulty
 def difficulty():
+    """
+    Choose difficulty between easy, normal or hard
+    """
     print('What difficulty do you crave warrior?\n')
     print('[Easy] - 100hp\n')
     print('[Normal] - 50hp\n')
@@ -40,25 +45,40 @@ def difficulty():
 
     if diff == 'easy':
         print('Difficulty Easy - 100hp set\n')
+        return 100
     elif diff == 'normal':
         print('Difficulty Normal - 50hp set\n')
+        return 50
     elif diff == 'hard':
         print('Difficulty Hard - 25hp set\n')
+        return 25
     else:
         print('Invalid difficulty. Try again\n')
-        difficulty()
+        return difficulty()
 
 
-class monster():
-    def __init__(self, name, description, health):
+# Player 
+player_name = get_player_name()
+player_hp = difficulty()
+player = Player(player_name, player_hp)
+
+
+# Monsters
+class Monster():
+    """
+    Creating monsters
+    """
+    def __init__(self, name, description, hp):
         self.name = name
         self.description = description
-        self.health = 0
+        self.hp = hp
 
-goblin = monster('Goblin', 'its wiry frame draped in tattered garments, with a crooked grin revealing jagged teeth and eyes gleaming with a mischievous glint', 5)
-ork = monster('Ork', 'a towering and brutishly built, its grizzled body marked with scars', 5)
-murloc = monster('Murloc', 'a small, amphibious creature with webbed appendages, emitting gurgling sounds', 5)
-cave_troll = monster('Cave Troll', 'a hulking figure draped in crude hides, its massive frame adorned with tangled mossy hair and thick, gnarled limbs clutching a stone club, its eyes glowing with a primal, fierce intensity', 5)
-forest_troll = monster('Forest Troll', 'a creature taller than the average human, its rugged, bark-like skin' 5)
-basilisk = monster('Basilisk', 'a serpentine creature with shimmering scales of iridescent hues, its piercing gaze freezing the air around it, a forked tongue flickering as it hisses, emanating an aura of petrifying danger' 5)
-wyvern = monster('Wyvern', 'a formidable winged creature with scaled skin shimmering in the dim light, razor-sharp talons gripping the cave floor as its leathery wings fold against its sleek body, keen eyes fixated on the intruder with a calculated intensity, ready to take flight at the slightest provocation' 5)
+
+goblin = Monster('Goblin', 'its wiry frame draped in tattered garments, with a crooked grin revealing jagged teeth and eyes gleaming with a mischievous glint', 5)
+ork = Monster('Ork', 'a towering and brutishly built, its grizzled body marked with scars', 5)
+murloc = Monster('Murloc', 'a small, amphibious creature with webbed appendages, emitting gurgling sounds', 5)
+cave_troll = Monster('Cave Troll', 'a hulking figure draped in crude hides, its massive frame adorned with tangled mossy hair and thick, gnarled limbs clutching a stone club, its eyes glowing with a primal, fierce intensity', 5)
+forest_troll = Monster('Forest Troll', 'a creature taller than the average human, its rugged, bark-like skin', 5)
+basilisk = Monster('Basilisk', 'a serpentine creature with shimmering scales of iridescent hues, its piercing gaze freezing the air around it, a forked tongue flickering as it hisses, emanating an aura of petrifying danger', 5)
+wyvern = Monster('Wyvern', 'a formidable winged creature with scaled skin shimmering in the dim light, razor-sharp talons gripping the cave floor as its leathery wings fold against its sleek body, keen eyes fixated on the intruder with a calculated intensity, ready to take flight at the slightest provocation', 5)
+
