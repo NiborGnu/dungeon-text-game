059589fc5fbc4d1d1ce28912f897946048aa740f
Robin Ung <Robin_ung@hotmail.com>
2024-01-11 16:58:01 +0000

    Add welcome text and main_menu

--- run.py
+++ run.py
@@ -4,14 +4,36 @@
 # Text based RPG Game
 # Made By Robin Ung
 
-import random
+from player import Player, get_player_name, difficulty
 import paths
-import player
 import time  # Add a timer function and a scoreboard
 from simple_term_menu import TerminalMenu
 
 
 def main_game():
-    paths.level_zero()
+    player_name = get_player_name()
+    player_hp = difficulty()
+    player = Player(player_name, player_hp)
+    paths.level_zero(player)
 
-main_game()
+
+print(f"""
+####################################
+#   Step into the Dungeon Game!    #
+# As a warrior, seek glory or face #
+#  the perilous embrace of death.  #
+#    Good luck on your journey!    #
+#    See you on the other side!    #
+####################################
+""")
+
+options = ['Start Game', 'How to play', 'Quit']
+main_menu = TerminalMenu(options)
+x = main_menu.show()
+
+if x == 0:
+    main_game()
+elif x == 1:
+    print('How to play: ...')
+elif x == 2:
+    print('Exiting the game. Goodbye!')
