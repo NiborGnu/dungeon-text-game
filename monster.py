64e98814da7c67ba54861753afa5386f3aaf7e00
Robin Ung <Robin_ung@hotmail.com>
2024-01-11 16:57:10 +0000

    Change monster.description text length

--- monster.py
+++ monster.py
@@ -23,14 +23,16 @@ eyes gleaming with a mischievous glint, """,
 ork = Monster(
     " Ork",
     f"""
-You enter a cave and you see a towering and brutishly built shape,
+You enter a cave and you see a towering
+and brutishly built shape,
 its grizzled body marked with scars, """,
     5
 )
 murloc = Monster(
     " Murloc",
     f"""
-You enter a dank cave with a pond in the middle and you see a small,
+You enter a dank cave with a pond
+in the middle and you see a small,
 amphibious creature with webbed appendages,
 emitting gurgling sounds, """,
     5
@@ -38,10 +40,11 @@ emitting gurgling sounds, """,
 cave_troll = Monster(
     " Cave Troll",
     f"""
-You enter a cave and you see a hulking figure draped in crude hides,
-its massive frame adorned with tangled mossy hair and thick,
+You enter a cave and you see a hulking figure
+draped in crude hides, its massive frame
+adorned with tangled mossy hair and thick,
 gnarled limbs clutching a stone club,
-its eyes glowing with a primal, fierce intensity, """,
+its eyes glowing with a intensity, """,
     5
 )
 forest_troll = Monster(
@@ -65,19 +68,22 @@ emanating an aura of petrifying danger, """,
 wyvern = Monster(
     " Wyvern",
     f"""
-You enter a big cave and you see a formidable winged creature,
-with scaled skin shimmering in the dim light, razor-sharp talons gripping
-the cave floor as its leathery wings fold against its sleek body,
-keen eyes fixated on the intruder with a calculated intensity,
-ready to take flight at the slightest provocation, """,
+You enter a big cave and you see a formidable
+winged creature, with scaled skin shimmering
+in the dim light, razor-sharp talons gripping
+the cave floor as its leathery wings fold against
+its sleek body, keen eyes fixated on the intruder
+with a calculated intensity, ready to take
+flight at the slightest provocation, """,
     7
 )
 black_dragon = Monster(
     " Black Dragon",
     f"""
-You enter a vast expanse of a cavern, shadows dance in the dim light.
-Massive scales shimmer, and fiery eyes gleam in the dark.
-A great ominous silhouette looms wrapped in midnight scales,
+You enter a vast expanse of a cavern, shadows
+dance in the dim light. Massive scales shimmer,
+and fiery eyes gleam in the dark. A great ominous
+silhouette looms wrapped in midnight scales,
 a force unmatched """,
     10
 )
