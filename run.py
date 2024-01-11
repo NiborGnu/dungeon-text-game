4118b1c72bfdceb64419d8f68059033d00dcac64
Robin Ung <Robin_ung@hotmail.com>
2024-01-11 14:59:24 +0000

    Move def player, difficulty, level_one, to own files

--- run.py
+++ run.py
@@ -5,90 +5,14 @@
 # Made By Robin Ung
 
 import random
-import monster
-import diceroll
+import paths
+import player
 import time  # Add a timer function and a scoreboard
 from simple_term_menu import TerminalMenu
 
 
 def main_game():
-    print(f'Hail {player.name} good to meet you!')
-    level_one()
+    print(f'Hail {player.player.name} good to meet you!')
+    paths.level_zero()
 
-
-# Player
-class Player():
-    """Player setup"""
-    def __init__(self, name, hp):
-        self.name = name
-        self.hp = hp
-
-
-def get_player_name():
-    """Get player name"""
-    print('Hail warrior! State your name: \n')
-    while True:
-        player_name = input('>')
-        if len(player_name) <= 20:
-            return player_name
-        else:
-            print(f"""
-I don't have room on my paper for that long a name!\n
-Do you have a nickname i can call you?(Max 20 characters)\n""")
-
-
-# Difficulty
-def difficulty():
-    """Choose difficulty between easy, normal or hard"""
-    print('What difficulty do you crave, warrior?\n')
-    diff = ['Easy - 100 HP', 'Normal - 50 HP', 'Hard - 25 HP']
-    difficulty_menu = TerminalMenu(diff)
-    x = difficulty_menu.show()
-
-    if x == 0:
-        print('Difficulty Easy - 100hp set\n')
-        return 100
-    elif x == 1:
-        print('Difficulty Normal - 50hp set\n')
-        return 50
-    elif x == 2:
-        print('Difficulty Hard - 25hp set\n')
-        return 25
-
-
-def level_one():
-    """First route player can take"""
-    print(f"{monster.ork.description} it's a it's an {monster.ork.name}")
-    diceroll.dice_roll(player, monster.ork)
-
-    if player.hp >= 0:
-        print(f"""
-You survived the encountor!\n
-Now you stand before a choice again will you go [left] or [right]?""")
-    path = ['Left', 'Right', 'Quit']
-    path_menu = TerminalMenu(path)
-
-    while True:
-        x = path_menu.show()
-        if x == 0:
-            print(f"{monster.murloc.description} it's a {monster.murloc.name}")
-            diceroll.dice_roll(player, monster.murloc)
-            if player.hp >= 0:
-                print(f"""
-You survived the encountor!\n
-Now you stand before a choice again will you go [left] or [right]?""")
-        elif x == 1:
-            print(f"{monster.goblin.description} it's a {monster.goblin.name}")
-            diceroll.dice_roll(player, monster.goblin)
-            if player.hp >= 0:
-                print(f"""
-You survived the encountor!\n
-Now you stand before a choice again will you go [left] or [right]?""")
-        elif x == 2:
-            print('quiting...')
-            # quit function
-
-player_name = get_player_name()
-player_hp = difficulty()
-player = Player(player_name, player_hp)
 main_game()
